3726ba3888df59d8f3aff9a5740ca0a62d02b780
Enno Boland <g@s01.de>
2017-11-05 12:02:56 +0100

    add void_auto_choose_pull_method.user.js script

---
+++ greasemonkey/void_auto_choose_pull_method.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        void_auto_choose_pull_method
+// @namespace   void
+// @include     https://github.com/voidlinux/void-packages/pull/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.addEventListener("load", () => {
+  document.querySelector(".merge-message button ~ button").click();
+  const commitCount = +document.getElementById("commits_tab_counter").innerText;
+  const commentCount = +document.getElementById("conversation_tab_counter").innerText;
+  
+  setTimeout(() => {
+    const pulltypes = document.querySelectorAll(".merge-message .select-menu-modal-holder .select-menu-item");
+    if(pulltypes.length != 3)
+      return alert("Must be three pulltypes!");
+    for(let x of pulltypes) {
+      const l = x.innerText.split('\n')[0].toLowerCase();
+      if(l.includes("rebase") && commitCount <= 3)
+        return x.click();
+      else if(l.includes("merge") && commitCount > 3)
+        return x.click();
+    }
+  }, 20);
+});
