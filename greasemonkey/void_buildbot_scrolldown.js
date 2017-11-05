87c36f36e655a9913f57a1e40c493a8a183cfa55
Enno Boland <g@s01.de>
2017-11-05 11:37:37 +0100

    greasemonkey: add script for auto scrolldown on buildbot

---
+++ greasemonkey/void_buildbot_scrolldown.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        void_buildbot_scrolldown
+// @namespace   void
+// @include     https://build.voidlinux.eu/builders/*/builds/*/steps/*/logs/stdio
+// @version     1
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+let timeout;
+
+function doScroll() {
+  window.scrollBy(0, 9999999);
+  timeout = setTimeout(doScroll, 100);
+}
+
+// wait 3 seconds so we are sure, the page isn't already finished
+timeout = setTimeout(doScroll, 3000);
+
+document.addEventListener("DOMContentLoaded", e => {
+    window.clearTimeout(timeout);
+});
