6207ed088c47ec56bdfe62e8b46adfaf0573313c
Enno Boland <g@s01.de>
2017-11-05 13:01:07 +0100

    Cosmetic changes to void_auto_choose_pull_method.user.js

--- greasemonkey/void_auto_choose_pull_method.user.js
+++ greasemonkey/void_auto_choose_pull_method.user.js
@@ -8,20 +8,26 @@
 // ==/UserScript==
 
 window.addEventListener("load", () => {
-  document.querySelector(".merge-message button ~ button").click();
-  const commitCount = +document.getElementById("commits_tab_counter").innerText;
-  const commentCount = +document.getElementById("conversation_tab_counter").innerText;
-  
-  setTimeout(() => {
-    const pulltypes = document.querySelectorAll(".merge-message .select-menu-modal-holder .select-menu-item");
-    if(pulltypes.length != 3)
-      return alert("Must be three pulltypes!");
-    for(let x of pulltypes) {
-      const l = x.innerText.split('\n')[0].toLowerCase();
-      if(l.includes("rebase") && commitCount <= 3)
-        return x.click();
-      else if(l.includes("merge") && commitCount > 3)
-        return x.click();
-    }
-  }, 20);
+	document.querySelector(".merge-message button ~ button").click();
+	const commitCount = +document.getElementById("commits_tab_counter").innerText;
+	const commentCount = +document.getElementById("conversation_tab_counter").innerText;
+
+	setTimeout(() => {
+		const pulltypes = document.querySelectorAll(".merge-message .select-menu-modal-holder .select-menu-item");
+		if(pulltypes.length != 3)
+			return alert("Must be three pulltypes!");
+
+		let method;
+		if(commitCount > 2)
+			method = 'merge';
+		else
+			method = 'rebase';
+
+		for(let x of pulltypes) {
+			const l = x.innerText.split('\n')[0].toLowerCase();
+			if(l.includes(method))
+				return x.click();
+		}
+		return alert(`No pull type ${method} found`);
+	}, 20);
 });
