e8cb3842961d7b4ef14272012a60ecbec4daec85
Enno Boland <g@s01.de>
2017-11-15 12:45:45 +0100

    buildbot scrolldown: allow to stop and restart autoscrolling

--- greasemonkey/void_buildbot_scrolldown.user.js
+++ greasemonkey/void_buildbot_scrolldown.user.js
@@ -8,16 +8,44 @@
 // @run-at      document-start
 // ==/UserScript==
 
-let timeout;
+let timeout = -1;
+let finished = false;
+let ignoreNext = false;
+
+document.scrollingElement.style.paddingBottom = '20px';
 
 function doScroll() {
-  window.scrollBy(0, 9999999);
-  timeout = setTimeout(doScroll, 100);
+  if(finished)
+    return;
+  const b = document.body;
+  b.style.background = "linear-gradient(to bottom, rgba(125,185,232,0) 80%,rgba(125,185,232,1) 100%) fixed";
+
+
+  ignoreNext = true;
+  timeout = setTimeout(doScroll, 500);
+  window.scrollTo(0, document.scrollingElement.scrollHeight);
+}
+function clearScroll() {
+  document.body.style.background = "";
+  window.clearTimeout(timeout);
+  timeout = -1;
 }
 
 // wait 3 seconds so we are sure, the page isn't already finished
 timeout = setTimeout(doScroll, 3000);
 
 document.addEventListener("DOMContentLoaded", e => {
-    window.clearTimeout(timeout);
+  finished = true;
+  clearScroll();
+});
+
+document.addEventListener("scroll", e => {
+  if(ignoreNext)
+    return ignoreNext = false;
+  const b = document.scrollingElement;
+  if (document.scrollingElement.scrollHeight - document.scrollingElement.clientHeight - e.pageY === 0 && timeout === -1) {
+    doScroll();
+  } else if(timeout !== -1) {
+    clearScroll();
+  }
 });
